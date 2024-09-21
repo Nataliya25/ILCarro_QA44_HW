cc221aa9e0cb19f1e87b5d1b72c73a85ce277e51
Nataliya25 <bnataliya13@gmail.com>
2024-09-21 10:57:20 +0300

    created emun class

--- src/main/java/pages/LetTheCarWorkPage.java
+++ src/main/java/pages/LetTheCarWorkPage.java
@@ -1,4 +1,14 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
 public class LetTheCarWorkPage extends BasePage{
+    public LetTheCarWorkPage (WebDriver driver){
+        setDriver(driver);
+        driver.get("https://ilcarro.web.app/search");
+        PageFactory.initElements(new AjaxElementLocatorFactory
+                (driver, 10), this);
+    }
 }
--- src/main/java/pages/TermsPage.java
+++ src/main/java/pages/TermsPage.java
@@ -1,5 +1,15 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
 public class TermsPage extends BasePage{
 
+    public TermsPage (WebDriver driver){
+        setDriver(driver);
+        driver.get("https://ilcarro.web.app/search");
+        PageFactory.initElements(new AjaxElementLocatorFactory
+                (driver, 10), this);
+    }
 }
